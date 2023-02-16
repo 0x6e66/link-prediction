64f03e3f60ccb9d4c92e8bd3bdea65bbc5901d35
Niklas Frondorf <niklas.frondorf@web.de>
2023-02-16 19:18:21 +0100

    removed static algo names in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 def compare_algorithms_for_reduced_graph(algos, reduced_graph, removed_edges, original_graph):
-    df_n1 = pd.DataFrame(columns=["source", "target", "rai", "jc", "aai", "pa",  "cnc"]) # "cnsh", "raish", "wiccn",
+    df_n1 = pd.DataFrame(columns=["source", "target"] + algos.keys())
     df_n1["source"], df_n1["target"] = list(zip(*removed_edges))
     for key in algos:
         df_n1[key] = list(algos[key](reduced_graph, removed_edges))[0][2]
 
 
-    df_g = pd.DataFrame(columns=["source", "target", "rai", "jc", "aai", "pa",  "cnc"]) # "cnsh", "raish", "wiccn",
+    df_g = pd.DataFrame(columns=["source", "target"] + algos.keys())
     df_g["source"], df_g["target"] = list(zip(*removed_edges))
     for key in algos:
         df_g[key] = list(algos[key](original_graph, removed_edges))[0][2]
 
-    delta = df_g.loc[:, ["rai", "jc", "aai", "pa",  "cnc"]].subtract(df_n1.loc[:, ["rai", "jc", "aai", "pa",  "cnc"]])
+    delta = df_g.loc[:, algos.keys()].subtract(df_n1.loc[:, algos.keys()])
     res = pd.concat([df_n1.loc[:, ["source", "target"]], delta], axis=1)
     return res
