d8f070ec66037c0b997cefc3ac7482c495c7a800
Niklas Frondorf <niklas.frondorf@web.de>
2023-02-19 20:31:49 +0100

    created funciton remove_edges in utils.py, tweaked on plotting and completed exercise 4

--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-import math
+import random
 
 '''
 Calculates the scores for every edge in 'removed_edges' for every algorithm in 'algos'
 Optional: With the option 'compare_with_original_graph' the scores for the 'reduced_graph' are being compared to the scores in the 'original_graph'
 '''
-
-
 def compare_normal_algorithms_for_reduced_graph(algos, reduced_graph, removed_edges):
     df_n = pd.DataFrame(
         columns=["edge", "is_removed_edge"] + list(algos.keys()))
@@ -41,8 +39,6 @@ def compare_normal_algorithms_for_reduced_graph(algos, reduced_graph, removed_ed
 Calculates the scores for every edge in 'removed_edges' for every algorithm in 'algos'
 Optional: With the option 'compare_with_original_graph' the scores for the 'reduced_graph' are being compared to the scores in the 'original_graph'
 '''
-
-
 def compare_community_algorithms_for_reduced_graph(algos, reduced_graph, removed_edges, communities):
     for i, community in enumerate(communities):
         for v in community:
@@ -84,8 +80,26 @@ def compare_combined(algos, reduced_graph, removed_edges, communities):
     return pd.concat([df1, df2.loc[:, list(algos["community_algos"].keys())]], axis=1)
 
 
-def plot_results(graph_num, result, scope=(0, 20_000), num_of_intervals=10):
-    assert (scope[0] < scope[1]+num_of_intervals)
+def remove_edges(graph, percentages):
+    reduced_graphs = []
+    removed_edges = []
+
+    for p in percentages:
+        num_of_edges_to_remove = int(graph.number_of_edges() * p)
+        edges_to_remove = random.sample(list(graph.edges()), k=num_of_edges_to_remove)
+        
+        removed_edges.append(edges_to_remove)
+
+        tmp_graph = graph.copy()
+        tmp_graph.remove_edges_from(edges_to_remove)
+        reduced_graphs.append(tmp_graph)
+
+    return reduced_graphs, removed_edges
+
+
+def plot_results(graph_num, result, scope=None, num_of_intervals=10):
+    if scope:
+        assert (scope[0] < scope[1]+num_of_intervals)
 
     algo_dict = {
         "rai": "Resource Allocation Index",
@@ -98,7 +112,7 @@ def plot_results(graph_num, result, scope=(0, 20_000), num_of_intervals=10):
         "wiccn": "Within- and Inter-Cluster Common Neighbors"
     }
 
-    barWidth = 0.1
+    bar_width = 0.1
     fig = plt.subplots(figsize=(20, 5))
 
     list_of_indexlists = []
@@ -113,18 +127,26 @@ def plot_results(graph_num, result, scope=(0, 20_000), num_of_intervals=10):
         list_of_indexlists.append(
             list(data[data['is_removed_edge'] == True].index))
 
-    width_of_interval = int((scope[1] - scope[0]) / num_of_intervals)
+    max_val = max(list(zip(*list_of_indexlists))[-1])
+    if scope:
+        width_of_interval = int((scope[1] - scope[0]) / num_of_intervals)
+    else:
+        width_of_interval = int(max_val / num_of_intervals)
+        scope = (0, max_val)
+
     for j in range(num_of_intervals):
         left, right = j*width_of_interval+1, (j+1)*width_of_interval
         disp_ranges.append(f"{left}-{right}")
         ranges.append((left, right))
 
-    max_val = max(list(zip(*list_of_indexlists))[-1])
     if scope[1] < max_val:
         disp_ranges[-1] = (f"{ranges[-1][1]}-{max_val}")
         ranges[-1] = (ranges[-1][1], max_val)
+    elif scope[1] > max_val:
+        disp_ranges[-1] = (f"{ranges[-1][0]}-{max_val}")
+        ranges[-1] = (ranges[-1][0], max_val)
 
-    for j, algo in enumerate(data.columns[2:]):
+    for j, algo in enumerate(result.columns[2:]):
         tmp_list = []
         for left, right in ranges:
             val_of_interval = len(
@@ -134,8 +156,8 @@ def plot_results(graph_num, result, scope=(0, 20_000), num_of_intervals=10):
 
     for j, algo in enumerate(result.columns[2:]):
         y = y_values_for_algorithms[j]
-        x = [x + barWidth*j for x in range(len(y))]
-        plt.bar(x, y, width=barWidth, edgecolor='grey', label=algo_dict[algo])
+        x = [x + bar_width*j for x in range(len(y))]
+        plt.bar(x, y, width=bar_width, edgecolor='grey', label=algo_dict[algo])
 
     plt.title(f"Graph n{graph_num}")
     plt.xlabel(xlabel=f"Rang berechnet von der Methode",
@@ -144,7 +166,12 @@ def plot_results(graph_num, result, scope=(0, 20_000), num_of_intervals=10):
     plt.ylabel(ylabel="Anzahl an zuvor gelöschten Kanten",
                fontweight='bold',
                fontsize=15)
-    plt.xticks([r + barWidth for r in range(num_of_intervals)], disp_ranges)
+    
+    num_of_algos = len(result.columns[2:])
+    if num_of_algos % 2 == 0:
+        plt.xticks([r + bar_width*(num_of_algos/2-0.5) for r in range(num_of_intervals)], disp_ranges)
+    elif num_of_algos % 2 != 0:
+        plt.xticks([r + bar_width*(num_of_algos/2-1) for r in range(num_of_intervals)], disp_ranges)
 
     plt.legend()
     plt.show()
